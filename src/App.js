27ff62807c11155b6bbb7076fa2427e9b57b2a8d
Rafael Ferreira Lemes <40215566+rflfl@users.noreply.github.com>
2023-12-21 13:52:56 +0000

    feat:ciradas funções soma, subtração e igual

--- src/App.js
+++ src/App.js
@@ -4,12 +4,57 @@ import { Container, Content, Row } from "./styles"
 import { useState } from "react"
 const App = () => {
   const [currentNumber, setCurrentNumber] = useState('0')
+  const [firstNumber, setFirstNumber] = useState('0')
+  const [operation, setOperation] = useState('')
+
   const handleOnClear = () => {
     setCurrentNumber('0')
+    setFirstNumber('0')
+    setOperation('')
   }
   const handleAddNumber = (number) => {
     setCurrentNumber(prev => `${number}${prev === '0' ? '' : prev}`)
   }
+
+  const handleSumNumbers = () => {
+    if (firstNumber === '0') {
+      setFirstNumber(currentNumber)
+      handleOnClear()
+      setOperation('+')
+    } else {
+      const sum =  Number(firstNumber) + Number(currentNumber)
+      setCurrentNumber(String(sum))
+      setOperation('')
+    }
+  }
+
+  const handleMinusNumbers = () => {
+    if (firstNumber === '0') {
+      setFirstNumber(currentNumber)
+      handleOnClear()
+      setOperation('-')
+    } else {
+      const subt =  Number(firstNumber) - Number(currentNumber)
+      setCurrentNumber(String(subt))
+      setOperation('')
+    }
+  }
+
+  const handleEquals = () => {
+    if (firstNumber !== '0' && operation !== '' && currentNumber !== '0') {
+      switch (operation) {
+        case '+':
+          handleSumNumbers()
+          break;
+        case '-':
+          handleMinusNumbers()
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
   return (
     <Container>
       <Content>
@@ -24,19 +69,19 @@ const App = () => {
           <Button label="7" onCLick={() => handleAddNumber('7')} />
           <Button label="8" onCLick={() => handleAddNumber('8')} />
           <Button label="9" onCLick={() => handleAddNumber('9')} />
-          <Button label="-" onCLick={() => handleAddNumber('-')} />
+          <Button label="-" onCLick={handleMinusNumbers} />
         </Row>
         <Row>
           <Button label="4" onCLick={() => handleAddNumber('4')} />
           <Button label="5" onCLick={() => handleAddNumber('5')} />
           <Button label="6" onCLick={() => handleAddNumber('6')} />
-          <Button label="+" onCLick={() => handleAddNumber('+')} />
+          <Button label="+" onCLick={handleSumNumbers} />
         </Row>
         <Row>
           <Button label="1" onCLick={() => handleAddNumber('1')} />
           <Button label="2" onCLick={() => handleAddNumber('2')} />
           <Button label="3" onCLick={() => handleAddNumber('3')} />
-          <Button label="=" onCLick={() => handleAddNumber('=')} />
+          <Button label="=" onCLick={handleEquals} />
         </Row>
       </Content>
     </Container>
